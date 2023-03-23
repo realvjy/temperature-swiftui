2ba014a7e8a546ccde65f8aa3b46d244cdcf77e1
realvjy <realvjy@gmail.com>
2023-03-24 01:06:43 +0530

    Initial Commit

---
+++ Temperature/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Temperature
+//
+//  Created by vijay verma on 24/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Temperature/TemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureApp.swift
+//  Temperature
+//
+//  Created by vijay verma on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
